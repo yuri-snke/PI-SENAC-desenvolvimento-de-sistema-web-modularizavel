3c9a0781944882b2d112622a1ceee77307a53b17
EduardoEliasFdeLima <edueuler.elias04@gmail.com>
2024-05-16 17:09:34 -0300

    alterando botão de login

--- src/data/repositories/loginRepository.js
+++ src/data/repositories/loginRepository.js
@@ -4,6 +4,7 @@ async function ValidarLogin(userLogin) {
   let comando = `SELECT * FROM tbl_usuario WHERE email = ? and senha = ?`;
   const [linhas] = await con.query(comando, [userLogin.email, userLogin.senha]);
 
+  
   return linhas;
 }
 
